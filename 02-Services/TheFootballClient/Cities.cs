b5d5ff7ebee0c2fbd15dff8b081d545b9845c657
Kazioslaw <113318007+Kazioslaw@users.noreply.github.com>
2022-10-11 14:59:55 +0200

    Generating club names files

---
+++ 02-Services/TheFootballClient/Cities.cs
@@ -0,0 +1,9 @@
+﻿namespace TheFootballClient
+{
+    public class Cities
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? unique_name { get; set; }
+    }
+}
---
+++ 02-Services/TheFootballClient/GenerationClubs.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+
+namespace TheFootballClient
+{
+    public class GenerationClubs
+    {
+        static void Generator()
+        {
+            var jsonCity = File.ReadAllText(@"Files\cities.json");
+            var cityJson = JsonSerializer.Deserialize<List<Cities>>(jsonCity);
+            List<string> cities = new List<string>();
+
+            foreach (var c in cityJson)
+            {
+                cities.Add($"KS {c.name}");
+            }
+
+        }
+
+    }
+}

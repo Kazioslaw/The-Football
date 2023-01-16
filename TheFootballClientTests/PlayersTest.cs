f7bb7ffdef1842974bc5c46160f86971832c30fd
Kazioslaw <113318007+Kazioslaw@users.noreply.github.com>
2023-01-16 16:56:00 +0100

    Little Fixes

--- TheFootballClientTests/PlayersTest.cs
+++ TheFootballClientTests/PlayersTest.cs
@@ -15,10 +15,11 @@ namespace TheFootballClientTests
         public void Generator_ValidCSVLocation_NamesCountAbove0()
         {
             //Arrange
+            string startupPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
             List<Player> players = new List<Player>();
             var playersGenerator = new Players();
-            var firstNameCSV = "";
-            var secondNameCSV = "";
+            var firstNameCSV = startupPath + @"Files\Imiona_Męskie.csv";
+            var secondNameCSV = startupPath + @"Files\Nazwiska_Męskie.csv";
             var testCountNumber = 0;
 
             //Act
